c174bf29281997c484d11b056b916ccf450921d8
olive <olive@Oliveira>
2026-04-10 08:43:12 -0300

    Criado projeto com maven e conctado ao Database postgresql

---
+++ pos-java-jdbc/src/main/java/conexao_jdbc/SingleConnection.java
@@ -0,0 +1,43 @@
+package conexao_jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class SingleConnection {
+	
+	private static String url = "jdbc:postgresql://localhost:5432/posjava";
+	private static String password = "admin";
+	private static String user = "postgres";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		try {
+			
+			if (connection == null) {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false);				
+				JOptionPane.showMessageDialog(null, "Conectou com Sucesso!");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+
+}
