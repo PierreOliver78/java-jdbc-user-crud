6d7192701c3272f6880dffc7d98c36fe4887019f
olive <olive@Oliveira>
2026-04-10 17:15:43 -0300

    Criando nosso INSERT ao banco de dados

--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -3,13 +3,25 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.Test;
 
 import conexao_jdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc{
 	
 	
 	@Test
 	public void initBanco(){
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(6L);
+		userposjava.setNome("Inês Teste");
+		userposjava.setEmail("inesteste@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
+		
 	}
+	
+	
 
 }
