e73d4420f62352710634a26765608a3c3bd9aadd
olive <olive@Oliveira>
2026-04-13 15:47:10 -0300

    Atualizando dados no banco de dados

--- pos-java-jdbc/src/main/java/dao/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -26,60 +26,81 @@ public class UserPosDAO {
 			insert.setString(2, userposjava.getNome());
 			insert.setString(3, userposjava.getEmail());
 			insert.execute();
-			connection.commit();//Salva no banco de dados
+			connection.commit();// Salva no banco de dados
 
 		} catch (Exception e) {
 			try {
-				connection.rollback();//Reverte a operação
-			} catch (SQLException e1) {				
+				connection.rollback();// Reverte a operação
+			} catch (SQLException e1) {
 				e1.printStackTrace();
 			}
 			e.printStackTrace();
 		}
 	}
-	
-	public List<Userposjava> listar() throws Exception{
+
+	public List<Userposjava> listar() throws Exception {
 		List<Userposjava> list = new ArrayList<Userposjava>();
-		
+
 		String sql = "select * from userposjava";
-		
+
 		PreparedStatement statement = connection.prepareStatement(sql);
 		ResultSet resultado = statement.executeQuery();
-		
+
 		while (resultado.next()) {
 			Userposjava userposjava = new Userposjava();
 			userposjava.setId(resultado.getLong("id"));
 			userposjava.setNome(resultado.getString("nome"));
 			userposjava.setEmail(resultado.getString("email"));
-			
+
 			list.add(userposjava);
-			
+
 		}
-		
+
 		return list;
-		
+
 	}
-	
-	public Userposjava buscar (Long id) throws Exception{
+
+	public Userposjava buscar(Long id) throws Exception {
 		Userposjava retorno = new Userposjava();
-		
+
 		String sql = "select * from userposjava where id = " + id;
-		
+
 		PreparedStatement statement = connection.prepareStatement(sql);
 		ResultSet resultado = statement.executeQuery();
-		
+
 		while (resultado.next()) {
-			
+
 			retorno.setId(resultado.getLong("id"));
 			retorno.setNome(resultado.getString("nome"));
 			retorno.setEmail(resultado.getString("email"));
-			
-			
-			
+
+		}
+
+		return retorno;
+
+	}
+
+	public void atualizar(Userposjava userposja) {
+		try {
+
+			String sql = "update userposjava set nome = ? where id = " + userposja.getId();
+
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, userposja.getNome());
+
+			statement.execute();
+			connection.commit();
+
+		} catch (Exception e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
 		}
 		
-		return retorno;
-				
 	}
 
 }
--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -4,59 +4,74 @@ import java.util.List;
 
 import org.junit.Test;
 
-import conexao_jdbc.SingleConnection;
 import dao.UserPosDAO;
 import model.Userposjava;
 
-public class TesteBancoJdbc{
-	
-	
+public class TesteBancoJdbc {
+
 	@Test
-	public void initBanco(){
+	public void initBanco() {
 		UserPosDAO userPosDAO = new UserPosDAO();
 		Userposjava userposjava = new Userposjava();
-		
+
 		userposjava.setId(6L);
 		userposjava.setNome("Inês Teste");
 		userposjava.setEmail("inesteste@gmail.com");
-		
+
 		userPosDAO.salvar(userposjava);
-		
+
 	}
-	
+
 	@Test
 	public void initListar() {
 		UserPosDAO dao = new UserPosDAO();
 		try {
 			List<Userposjava> list = dao.listar();
-			
+
 			for (Userposjava userposjava : list) {
 				System.out.println(userposjava);
 				System.out.println("-------------------------------");
 			}
-			
-		} catch (Exception e) {			
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}		
-		
+		}
+
 	}
-	
+
 	@Test
 	public void initBuscar() {
-		
+
 		UserPosDAO dao = new UserPosDAO();
-		
+
 		try {
 			Userposjava userposjava = dao.buscar(5L);
 			System.out.println(userposjava);
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
+
+	@Test
+	public void initAtualizar() {
+
+		try {
+
+			UserPosDAO dao = new UserPosDAO();
+
+			Userposjava objetoBanco = dao.buscar(6L);
+
+			objetoBanco.setNome("Inês Teste");
+
+			dao.atualizar(objetoBanco);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
