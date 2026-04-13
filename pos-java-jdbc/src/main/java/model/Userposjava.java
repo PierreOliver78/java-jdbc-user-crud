fe9ba79379d249962644343b1f094fcdaad6aa5a
olive <olive@Oliveira>
2026-04-13 14:30:22 -0300

    Criando nossa consulta SQL ao banco de dados

--- pos-java-jdbc/src/main/java/model/Userposjava.java
+++ pos-java-jdbc/src/main/java/model/Userposjava.java
@@ -26,6 +26,12 @@ public class Userposjava {
 		this.email = email;
 	}
 	
+	@Override
+	public String toString() {
+		return "Userposjava [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+	
+	
 	
 
 }
